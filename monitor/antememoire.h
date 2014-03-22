b6238d6806308c33283d3e07010fd3d47ecb5393
dupgit <olivier.delhomme@free.fr>
2014-03-22 19:05:36 +0100

    Add antememoire.[ch] files. Configured build system accordingly.

---
+++ monitor/antememoire.h
@@ -0,0 +1,30 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*- */
+/*
+ *    antememoire.h
+ *    This file is part of "Sauvegarde" project.
+ *
+ *    (C) Copyright 2014 Olivier Delhomme
+ *     e-mail : olivier.delhomme@free.fr
+ *
+ *    "Sauvegarde" is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    "Sauvegarde" is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with "Sauvegarde".  If not, see <http://www.gnu.org/licenses/>
+ */
+/**
+ * @file antememoire.h
+ *
+ *  This file contains all the definitions for the ciseaux program.
+ */
+#ifndef _ANTEMEMOIRE_H_
+#define _ANTEMEMOIRE_H_
+
+#endif /* #ifndef _ANTEMEMOIRE_H_ */
