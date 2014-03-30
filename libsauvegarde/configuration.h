eee68b144113ea704190d83fefb960531d3d0c43
dupgit <olivier.delhomme@free.fr>
2014-03-30 22:52:39 +0200

    Cut into small function read_from_configuration_file function.
    This permits to reduce McCabe cyclomatic complexity from 16
    to less than 10.

--- libsauvegarde/configuration.h
+++ libsauvegarde/configuration.h
@@ -98,4 +98,30 @@
 extern gchar *get_probable_etc_path(gchar *progname);
 
 
+/**
+ * Reads a string from keyname key in the group grouname from keyfile file
+ * and displays errormsg in case of an error
+ * @param keyfile : the opened keyfile to read from
+ * @param filename : the filename of the keyfile file
+ * @param groupname : the groupname where to look for the key
+ * @param keyname : the key to read the string from
+ * @param errormsg : the error message to be displayed in case of an error
+ * @returns the string read at the keyname in the groupname of keyfile
+ *          file.
+ */
+gchar *read_string_from_file(GKeyFile *keyfile, gchar *filename, gchar *groupname, gchar *keyname, gchar *errormsg);
+
+/**
+ * Reads a gint64 from keyname key in the group grouname from keyfile file
+ * and displays errormsg in case of an error
+ * @param keyfile : the opened keyfile to read from
+ * @param filename : the filename of the keyfile file
+ * @param groupname : the groupname where to look for the key
+ * @param keyname : the key to read the gint64 from
+ * @param errormsg : the error message to be displayed in case of an error
+ * @returns the gint64 read at the keyname in the groupname of keyfile
+ *          file or 0;
+ */
+gint64 read_int64_from_file(GKeyFile *keyfile, gchar *filename, gchar *groupname, gchar *keyname, gchar *errormsg);
+
 #endif /* #ifndef _CONFIGURATION_H_ */
