ab3c2b6bba521011c3125056b6bf1ef2dcc5286c
dupgit <olivier.delhomme@free.fr>
2014-05-01 18:54:35 +0200

    Packing system skeleton.

--- libsauvegarde/libsauvegarde.h
+++ libsauvegarde/libsauvegarde.h
@@ -38,6 +38,7 @@
 #include <glib/gi18n-lib.h>
 #include <zmq.h>
 #include <sqlite3.h>
+#include <msgpack.h>
 
 #include "configuration.h"
 #include "communique.h"
---
+++ libsauvegarde/packing.h
@@ -0,0 +1,32 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*- */
+/*
+ *    packing.h
+ *    This file is part of "Sauvegarde" project.
+ *
+ *    (C) Copyright 2014 Olivier Delhomme
+ *     e-mail : olivier.delhomme@free.fr
+ *
+ *    "Sauvegarde" is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    "Sauvegarde" is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with "Sauvegarde".  If not, see <http://www.gnu.org/licenses/>
+ */
+/**
+ * @file packing.h
+ *
+ * This file contains all the definitions of the functions and structures
+ * to pack and unpack messages for all the programs of "Sauvegarde"
+ * project.
+ */
+#ifndef _PACKING_H_
+#define _PACKING_H_
+
+#endif /* #ifndef _PACKING_H_ */
