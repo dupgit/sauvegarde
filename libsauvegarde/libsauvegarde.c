b7366a67f795de21238e4f280ed9d2f254b8d89c
dupgit <olivier.delhomme@free.fr>
2015-03-13 21:06:32 +0100

    Errors should be printed even if DEBUG is not enabled.

--- libsauvegarde/libsauvegarde.c
+++ libsauvegarde/libsauvegarde.c
@@ -311,13 +311,12 @@ void print_error(char *filename, int lineno, const char *format, ...)
 {
     va_list ap;
 
-    if (ENABLE_DEBUG == TRUE)
-        {
-            fprintf(stderr, "[%s, %d] ", filename, lineno);
-            va_start(ap, format);
-            vfprintf(stderr, format, ap);
-            va_end(ap);
-        }
+
+    fprintf(stderr, "[%s, %d] ", filename, lineno);
+    va_start(ap, format);
+    vfprintf(stderr, format, ap);
+    va_end(ap);
+
 }
 
 
