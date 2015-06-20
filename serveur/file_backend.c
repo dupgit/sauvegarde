d3e5d3ebc5110a10715b81a1fd25d0f8ba3622f8
dupgit <olivier.delhomme@free.fr>
2015-06-20 15:23:05 +0200

    Corrects a bug that occurred when trying to get file's hashs on a directory.

--- serveur/file_backend.c
+++ serveur/file_backend.c
@@ -573,19 +573,23 @@ static meta_data_t *extract_from_line(gchar *line, GRegex *a_regex)
                     i = 0;
                     hash_list = NULL;
 
-                    while (hashs[i] != NULL)
+                    if (hashs != NULL)
                         {
-                            a_hash = g_strndup(g_strchug(hashs[i] + 1), strlen(g_strchug(hashs[i])) - 2);
+                            while (hashs[i] != NULL)
+                                {
+                                    a_hash = g_strndup(g_strchug(hashs[i] + 1), strlen(g_strchug(hashs[i])) - 2);
 
-                            /* we have to base64 encode it to insert it into the meta_data_t * structure */
-                            hash_list = g_slist_prepend(hash_list, g_base64_decode(a_hash, &len));
-                            free_variable(a_hash);
-                            i = i + 1;
+                                    /* we have to base64 encode it to insert it into the meta_data_t * structure */
+                                    hash_list = g_slist_prepend(hash_list, g_base64_decode(a_hash, &len));
+                                    free_variable(a_hash);
+                                    i = i + 1;
+                                }
+
+                            g_strfreev(hashs);
+
+
+                            hash_list = g_slist_reverse(hash_list);
                         }
-
-                    g_strfreev(hashs);
-
-                    hash_list = g_slist_reverse(hash_list);
 
                     meta->hash_list = hash_list;
 
