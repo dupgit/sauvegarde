76f44864c5e8874c305a459c2e652be2da8264ff
dupgit <olivier.delhomme@free.fr>
2015-11-12 22:58:07 +0100

    No need to bother to use sudo to get fanotify's interface: it isn't implemented in travis-ci.org!

--- tests/launch_tests.bash
+++ tests/launch_tests.bash
@@ -23,37 +23,35 @@
 
 # All paths
 
-export HOMEDIR=$1
-
 # Launching the server
-$HOMEDIR/local/bin/cdpfglserver -c $HOMEDIR/build/dupgit/sauvegarde/tests/server.conf &
+$HOME/local/bin/cdpfglserver -c $HOME/build/dupgit/sauvegarde/tests/server.conf &
 
 # Waiting for the server to achieve directories creation
 sleep 40s
 
 # Launching the client (must be root to be able to use fanotify).
-$HOMEDIR/local/bin/cdpfglclient -c $HOMEDIR/build/dupgit/sauvegarde/tests/client.conf &
+$HOME/local/bin/cdpfglclient -c $HOME/build/dupgit/sauvegarde/tests/client.conf &
 
 # Waiting that the client finishes it's first pass (should be quick here).
-sleep 10s
+sleep 1s
 
-# Creating files in the monitored directory
-dd if=/dev/zero of=$HOMEDIR/build/dupgit/sauvegarde/tests/zerofile.dd count=3 bs=16k
-dd if=/dev/urandom of=$HOMEDIR/build/dupgit/sauvegarde/tests/urandomfile.dd count=3 bs=16k
+# Creating files in the monitored directory (commented out because fanotify is not implemented in travis-ci)
+dd if=/dev/zero of=$HOME/build/dupgit/sauvegarde/tests/zerofile.dd count=3 bs=16k
+#dd if=/dev/urandom of=$HOME/build/dupgit/sauvegarde/tests/urandomfile.dd count=3 bs=16k
 
 sleep 5s
 
-md5sum $HOMEDIR/build/dupgit/sauvegarde/tests/d2/file_with_repetitions >$HOMEDIR/md5sums
-md5sum $HOMEDIR/build/dupgit/sauvegarde/tests/urandomfile.dd >>$HOMEDIR/md5sums
+md5sum $HOME/build/dupgit/sauvegarde/tests/d2/file_with_repetitions >$HOME/md5sums
+#md5sum $HOME/build/dupgit/sauvegarde/tests/urandomfile.dd >>$HOME/md5sums
 
-# Trying to restore two files.
-$HOMEDIR/local/bin/cdpfglrestore -c $HOMEDIR/build/dupgit/sauvegarde/tests/restore.conf -r d2/file_with_repetitions$ -w $HOMEDIR/
-$HOMEDIR/local/bin/cdpfglrestore -c $HOMEDIR/build/dupgit/sauvegarde/tests/restore.conf -r urandomfile.dd$ -w $HOMEDIR/
+# Trying to restore one file (the other one is not created)
+$HOME/local/bin/cdpfglrestore -c $HOME/build/dupgit/sauvegarde/tests/restore.conf -r d2/file_with_repetitions$ -w $HOME/
+#$HOME/local/bin/cdpfglrestore -c $HOME/build/dupgit/sauvegarde/tests/restore.conf -r urandomfile.dd$ -w $HOME/
 
-md5sum $HOMEDIR/file_with_repetitions >>$HOMEDIR/md5sums
-md5sum $HOMEDIR/urandomfile.dd >>$HOMEDIR/md5sums
+md5sum $HOME/file_with_repetitions >>$HOME/md5sums
+# md5sum $HOME/urandomfile.dd >>$HOME/md5sums
 
-cat $HOMEDIR/md5sums
+cat $HOME/md5sums
 
 # Waiting a bit before killing the programs
 sleep 1s
