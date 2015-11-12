a2562b73eda90268c1889c67dcdedb4018a12cf6
dupgit <olivier.delhomme@free.fr>
2015-11-12 22:38:16 +0100

    sudo in .travis.yml instead of the script itself ?

--- tests/launch_tests.bash
+++ tests/launch_tests.bash
@@ -23,35 +23,37 @@
 
 # All paths
 
+export $HOMEDIR=$1
+
 # Launching the server
-$HOME/local/bin/cdpfglserver -c $HOME/build/dupgit/sauvegarde/tests/server.conf &
+$HOMEDIR/local/bin/cdpfglserver -c $HOMEDIR/build/dupgit/sauvegarde/tests/server.conf &
 
 # Waiting for the server to achieve directories creation
 sleep 40s
 
 # Launching the client (must be root to be able to use fanotify).
-sudo $HOME/local/bin/cdpfglclient -c $HOME/build/dupgit/sauvegarde/tests/client.conf &
+$HOMEDIR/local/bin/cdpfglclient -c $HOMEDIR/build/dupgit/sauvegarde/tests/client.conf &
 
 # Waiting that the client finishes it's first pass (should be quick here).
 sleep 10s
 
 # Creating files in the monitored directory
-dd if=/dev/zero of=$HOME/build/dupgit/sauvegarde/tests/zerofile.dd count=3 bs=16k
-dd if=/dev/urandom of=$HOME/build/dupgit/sauvegarde/tests/urandomfile.dd count=3 bs=16k
+dd if=/dev/zero of=$HOMEDIR/build/dupgit/sauvegarde/tests/zerofile.dd count=3 bs=16k
+dd if=/dev/urandom of=$HOMEDIR/build/dupgit/sauvegarde/tests/urandomfile.dd count=3 bs=16k
 
 sleep 5s
 
-md5sum $HOME/build/dupgit/sauvegarde/tests/d2/file_with_repetitions >$HOME/md5sums
-md5sum $HOME/build/dupgit/sauvegarde/tests/urandomfile.dd >>$HOME/md5sums
+md5sum $HOMEDIR/build/dupgit/sauvegarde/tests/d2/file_with_repetitions >$HOMEDIR/md5sums
+md5sum $HOMEDIR/build/dupgit/sauvegarde/tests/urandomfile.dd >>$HOMEDIR/md5sums
 
 # Trying to restore two files.
-$HOME/local/bin/cdpfglrestore -c $HOME/build/dupgit/sauvegarde/tests/restore.conf -r d2/file_with_repetitions$ -w $HOME/
-$HOME/local/bin/cdpfglrestore -c $HOME/build/dupgit/sauvegarde/tests/restore.conf -r urandomfile.dd$ -w $HOME/
+$HOMEDIR/local/bin/cdpfglrestore -c $HOMEDIR/build/dupgit/sauvegarde/tests/restore.conf -r d2/file_with_repetitions$ -w $HOMEDIR/
+$HOMEDIR/local/bin/cdpfglrestore -c $HOMEDIR/build/dupgit/sauvegarde/tests/restore.conf -r urandomfile.dd$ -w $HOMEDIR/
 
-md5sum $HOME/file_with_repetitions >>$HOME/md5sums
-md5sum $HOME/urandomfile.dd >>$HOME/md5sums
+md5sum $HOMEDIR/file_with_repetitions >>$HOMEDIR/md5sums
+md5sum $HOMEDIR/urandomfile.dd >>$HOMEDIR/md5sums
 
-cat $HOME/md5sums
+cat $HOMEDIR/md5sums
 
 # Waiting a bit before killing the programs
 sleep 1s
