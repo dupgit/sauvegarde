a22953e33db778210862f875fb5c20bd6538070b
dupgit <olivier.delhomme@free.fr>
2016-01-31 22:36:21 +0100

    Removes old names of dircetories to new ones in order to calculate complexity correctly.

--- tools/complexity.bash
+++ tools/complexity.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 export PATH_TO_LIZARD="../../projets_externes/lizard/lizard"
-$PATH_TO_LIZARD -w -s cyclomatic_complexity -C 10 ../monitor/ ../serveur/ ../libsauvegarde/ ../restaure/
+$PATH_TO_LIZARD -s cyclomatic_complexity -C 10 ../client/ ../server/ ../libcdpfgl/ ../restore/
