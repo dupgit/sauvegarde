c30e60c876249bd8da41d44e197de310b0dc585d
dupgit <olivier.delhomme@free.fr>
2016-09-07 21:57:52 +0200

    Comments generate.bash script to indicate its purpose.

--- man/generate.bash
+++ man/generate.bash
@@ -1,7 +1,9 @@
 #!/bin/bash
+#
+# This scripts generates man pages with the help of
+# pandoc program from the markdown files (.md)
 
-PANDOC_CMD="pandoc -s -f markdown -t man" 
-# -V adjusting=b"
+PANDOC_CMD="pandoc -s -f markdown -t man -V adjusting=b"
 
 for prog in cdpfglserver cdpfglclient cdpfglrestore; do
 	${PANDOC_CMD} -o ${prog}.1 ${prog}.md
